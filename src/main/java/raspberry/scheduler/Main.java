3fbedd4c82449de27392b92c35acff61b9f45564
Alan <69041772+forestsky1@users.noreply.github.com>
2021-07-31 13:34:56 +1200

    placeholder classes

--- src/main/java/raspberry/scheduler/Main.java
+++ src/main/java/raspberry/scheduler/Main.java
@@ -1,8 +1,19 @@
 package main.java.raspberry.scheduler;
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application{
+
 
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello world");
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
     }
 }
---
+++ src/main/java/raspberry/scheduler/model/StartScreen.java
@@ -0,0 +1,4 @@
+package main.java.raspberry.scheduler.model;
+
+public class StartScreen {
+}
