aeeb5691f014745d3978d17106bdb82dbf4d65ee
Young Min Kwon <ymkwon1@hotmail.com>
2021-08-05 16:30:31 +1200

    remove some print statements

--- src/main/java/raspberry/scheduler/cli/CLIRunner.java
+++ src/main/java/raspberry/scheduler/cli/CLIRunner.java
@@ -4,12 +4,11 @@ import raspberry.scheduler.cli.CLIConfig;
 
 public class CLIRunner {
     public static void main(String[] inputs) throws ParserException {
-        System.out.println("Hello world");
-
 //        for (String input: inputs){
 //            System.out.println(input);
 //        }
 
         CLIConfig CLIConfig = CLIParser.parser(inputs);
+
     }
 }
