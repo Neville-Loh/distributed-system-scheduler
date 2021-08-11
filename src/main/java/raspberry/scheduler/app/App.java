b7c80d110813f6ac87f3a103326ddc3d5e648056
Alan <69041772+forestsky1@users.noreply.github.com>
2021-08-12 04:42:29 +1200

    Deleted unused classes and FXML files- Deleted ScreenController Deleted StartScreenController Deleted Old MainView/.fxml Deleted StartScreenView.fxml

--- src/main/java/raspberry/scheduler/app/App.java
+++ src/main/java/raspberry/scheduler/app/App.java
@@ -24,7 +24,7 @@ public class App extends Application{
 
         @Override
         public void start(Stage primaryStage) throws Exception {
-            Parent root = FXMLLoader.load(getClass().getResource("/view/MainView2.fxml")
+            Parent root = FXMLLoader.load(getClass().getResource("/view/MainView.fxml")
             );
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
