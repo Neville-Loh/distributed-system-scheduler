79152478f8659e81a1950a13513cf4d1cd0e7970
Alan <forestsky@me.com>
2021-08-12 16:54:09 +1200

    Gantt chart now visualised without polling currently has placeholder data Needs to be formatted for css style

--- src/main/java/raspberry/scheduler/app/visualisation/controller/MainController.java
+++ src/main/java/raspberry/scheduler/app/visualisation/controller/MainController.java
@@ -3,12 +3,17 @@ package raspberry.scheduler.app.visualisation.controller;
 import eu.hansolo.tilesfx.Tile;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.collections.FXCollections;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import raspberry.scheduler.app.App;
 import raspberry.scheduler.app.visualisation.Updater;
 import raspberry.scheduler.app.visualisation.model.GanttChart;
 import raspberry.scheduler.cli.CLIConfig;
-
+import java.util.Arrays;
+import javafx.scene.chart.XYChart;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -25,6 +30,8 @@ public class MainController implements Initializable {
     private Label _inputFile, _outputFile, _numProcessors, _numCores, _timeElapsed, _iterations,_status;
     @FXML
     private Tile _memTile;
+    @FXML
+    private VBox _ganttBox;
 
     private CLIConfig _config;
     private String _inputFileName;
@@ -32,12 +39,16 @@ public class MainController implements Initializable {
     private GanttChart _ganttChart;
     private int _numSchedules;
     private Updater _updater;
+    private GanttChart _gantChart;
+    private NumberAxis _xAxis;
+    private CategoryAxis _yAxis;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         _config = App.GetCLIConfig();
         setIdleStats();
         setupMemTile();
+        setUpGanttChart();
         _updater = new Updater(_timeElapsed, _iterations,_status, _memTile);
 
     }
@@ -55,6 +66,51 @@ public class MainController implements Initializable {
         _memTile.setBarColor(rgb(255,255,255));
         _memTile.setThresholdColor(rgb(255,255,255));
         _memTile.setTickLabelDecimals(0);
+
+    }
+
+    private void setUpGanttChart(){
+        String[] processors = new String[]{"1","2","3","4"};
+
+
+        NumberAxis xAxis = new NumberAxis();
+       CategoryAxis yAxis = new CategoryAxis();
+        GanttChart<Number,String> _ganttChart = new GanttChart<Number,String>(xAxis,yAxis);
+        xAxis.setLabel("");
+        //xAxis.setTickLabelFill(Color.CHOCOLATE);
+        xAxis.setMinorTickCount(4);
+
+        yAxis.setLabel("");
+       // yAxis.setTickLabelFill(Color.CHOCOLATE);
+        yAxis.setTickLabelGap(10);
+        yAxis.setCategories(FXCollections.<String>observableArrayList(Arrays.asList(processors)));
+
+        _ganttChart.setTitle("Please work for the love of god");
+        _ganttChart.setLegendVisible(false);
+        _ganttChart.setBlockHeight( 50);
+        String processor = processors[0];
+        XYChart.Series series1 = new XYChart.Series();
+        series1.getData().add(new XYChart.Data(0, processor, new GanttChart.Attributes( 1, "status-green","2")));
+        series1.getData().add(new XYChart.Data(1, processor, new GanttChart.Attributes( 1, "status-green","2")));
+        series1.getData().add(new XYChart.Data(2, processor, new GanttChart.Attributes( 1, "status-green","2")));
+        series1.getData().add(new XYChart.Data(3, processor, new GanttChart.Attributes( 1, "status-green","2")));
+
+        processor = processors[1];
+        XYChart.Series series2 = new XYChart.Series();
+        series2.getData().add(new XYChart.Data(0, processor, new GanttChart.Attributes( 1, "status-green","2")));
+        series2.getData().add(new XYChart.Data(1, processor, new GanttChart.Attributes( 1, "status-green","2")));
+        series2.getData().add(new XYChart.Data(2, processor, new GanttChart.Attributes( 1, "status-green","2")));
+
+        processor = processors[2];
+        XYChart.Series series3 = new XYChart.Series();
+        series3.getData().add(new XYChart.Data(0, processor, new GanttChart.Attributes( 1, "status-green","2")));
+        series3.getData().add(new XYChart.Data(1, processor, new GanttChart.Attributes( 1, "status-green","2")));
+        series3.getData().add(new XYChart.Data(3, processor, new GanttChart.Attributes( 1, "status-green","2")));
+
+        _ganttChart.getData().addAll(series1, series2, series3);
+
+        _ganttBox.getChildren().add(_ganttChart);
+        _ganttChart.getStylesheets().add(getClass().getResource("/view/css/gantt.css").toExternalForm());
 
     }
 
--- src/main/java/raspberry/scheduler/app/visualisation/model/GanttChart.java
+++ src/main/java/raspberry/scheduler/app/visualisation/model/GanttChart.java
@@ -3,8 +3,11 @@ package raspberry.scheduler.app.visualisation.model;
 import javafx.beans.NamedArg;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.chart.*;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Rectangle;
 
@@ -12,7 +15,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class GanttChart<X,Y> extends XYChart<X,Y> {
+public class GanttChart<X, Y> extends XYChart<X, Y> {
     /**
      * Constructs a XYChart given the two axes. The initial content for the chart
      * plot background and plot area that includes vertical and horizontal grid
@@ -20,7 +23,7 @@ public class GanttChart<X,Y> extends XYChart<X,Y> {
      *
      * @param axis  X Axis for this XY chart
      * @param axis2 Y Axis for this XY chart
-     * Gantt chart adapted from https://stackoverflow.com/questions/27975898/gantt-chart-from-scratch
+     *              Gantt chart adapted from https://stackoverflow.com/questions/27975898/gantt-chart-from-scratch
      */
 
     public static class Attributes {
@@ -31,9 +34,9 @@ public class GanttChart<X,Y> extends XYChart<X,Y> {
 
 
         /**
-         * @param   lengthMs - lenth (time) for task
+         * @param lengthMs   - lenth (time) for task
          * @param styleClass - style class for scheduled task
-         * @param taskNum - the task number for the given task
+         * @param taskNum    - the task number for the given task
          */
 
         public Attributes(long lengthMs, String styleClass, String taskNum) {
@@ -42,15 +45,23 @@ public class GanttChart<X,Y> extends XYChart<X,Y> {
             _styleClass = styleClass;
             _taskNum = taskNum;
         }
+
+        public String getTaskNum() {
+            return _taskNum;
+        }
+
         public long getLength() {
             return _length;
         }
+
         public void setLength(long length) {
             _length = length;
         }
+
         public String getStyleClass() {
             return _styleClass;
         }
+
         public void setStyleClass(String styleClass) {
             _styleClass = styleClass;
         }
@@ -63,7 +74,7 @@ public class GanttChart<X,Y> extends XYChart<X,Y> {
         this(xAxis, yAxis, FXCollections.<Series<X, Y>>observableArrayList());
     }
 
-    public GanttChart(@NamedArg("xAxis") Axis<X> xAxis, @NamedArg("yAxis") Axis<Y> yAxis, @NamedArg("data") ObservableList<Series<X,Y>> data) {
+    public GanttChart(@NamedArg("xAxis") Axis<X> xAxis, @NamedArg("yAxis") Axis<Y> yAxis, @NamedArg("data") ObservableList<Series<X, Y>> data) {
         super(xAxis, yAxis);
         if (!(xAxis instanceof ValueAxis && yAxis instanceof CategoryAxis)) {
             throw new IllegalArgumentException("Axis type incorrect, X and Y should both be NumberAxis");
@@ -71,23 +82,28 @@ public class GanttChart<X,Y> extends XYChart<X,Y> {
         setData(data);
     }
 
-    private static String getStyleClass( Object obj) {
+    private static String getStyleClass(Object obj) {
         return ((Attributes) obj).getStyleClass();
     }
 
-    private static double getLength( Object obj) {
+    private static double getLength(Object obj) {
         return ((Attributes) obj).getLength();
     }
 
-    @Override protected void layoutPlotChildren() {
+    private static String getTaskNum(Object obj) {
+        return ((Attributes) obj).getTaskNum();
+    }
 
-        for (int seriesIndex=0; seriesIndex < getData().size(); seriesIndex++) {
+    @Override
+    protected void layoutPlotChildren() {
 
-            Series<X,Y> series = getData().get(seriesIndex);
+        for (int seriesIndex = 0; seriesIndex < getData().size(); seriesIndex++) {
 
-            Iterator<Data<X,Y>> iter = getDisplayedDataIterator(series);
-            while(iter.hasNext()) {
-                Data<X,Y> item = iter.next();
+            Series<X, Y> series = getData().get(seriesIndex);
+
+            Iterator<Data<X, Y>> iter = getDisplayedDataIterator(series);
+            while (iter.hasNext()) {
+                Data<X, Y> item = iter.next();
                 double x = getXAxis().getDisplayPosition(item.getXValue());
                 double y = getYAxis().getDisplayPosition(item.getYValue());
                 if (Double.isNaN(x) || Double.isNaN(y)) {
@@ -97,16 +113,16 @@ public class GanttChart<X,Y> extends XYChart<X,Y> {
                 Rectangle ellipse;
                 if (block != null) {
                     if (block instanceof StackPane) {
-                        StackPane region = (StackPane)item.getNode();
+                        StackPane region = (StackPane) item.getNode();
                         if (region.getShape() == null) {
-                            ellipse = new Rectangle( getLength( item.getExtraValue()), getBlockHeight());
+                            ellipse = new Rectangle(getLength(item.getExtraValue()), getBlockHeight());
                         } else if (region.getShape() instanceof Rectangle) {
-                            ellipse = (Rectangle)region.getShape();
+                            ellipse = (Rectangle) region.getShape();
                         } else {
                             return;
                         }
-                        ellipse.setWidth( getLength( item.getExtraValue()) * ((getXAxis() instanceof NumberAxis) ? Math.abs(((NumberAxis)getXAxis()).getScale()) : 1));
-                        ellipse.setHeight(getBlockHeight() * ((getYAxis() instanceof NumberAxis) ? Math.abs(((NumberAxis)getYAxis()).getScale()) : 1));
+                        ellipse.setWidth(getLength(item.getExtraValue()) * ((getXAxis() instanceof NumberAxis) ? Math.abs(((NumberAxis) getXAxis()).getScale()) : 1));
+                        ellipse.setHeight(getBlockHeight() * ((getYAxis() instanceof NumberAxis) ? Math.abs(((NumberAxis) getYAxis()).getScale()) : 1));
                         y -= getBlockHeight() / 2.0;
 
                         // Note: workaround for RT-7689 - saw this in ProgressControlSkin
@@ -117,6 +133,11 @@ public class GanttChart<X,Y> extends XYChart<X,Y> {
                         region.setScaleShape(false);
                         region.setCenterShape(false);
                         region.setCacheShape(false);
+
+                        Label num = new Label(getTaskNum(item.getExtraValue()));
+                       // num.minWidthProperty().bind(ellipse.widthProperty());
+                        num.setPadding(new Insets(ellipse.getHeight()/2,0,0,ellipse.getWidth()/2));
+                        region.getChildren().add(num);
 
                         block.setLayoutX(x);
                         block.setLayoutY(y);
@@ -130,34 +151,39 @@ public class GanttChart<X,Y> extends XYChart<X,Y> {
         return blockHeight;
     }
 
-    public void setBlockHeight( double blockHeight) {
+    public void setBlockHeight(double blockHeight) {
         this.blockHeight = blockHeight;
     }
 
-    @Override protected void dataItemAdded(Series<X,Y> series, int itemIndex, Data<X,Y> item) {
+    @Override
+    protected void dataItemAdded(Series<X, Y> series, int itemIndex, Data<X, Y> item) {
         Node block = createContainer(series, getData().indexOf(series), item, itemIndex);
         getPlotChildren().add(block);
     }
 
-    @Override protected  void dataItemRemoved(final Data<X,Y> item, final Series<X,Y> series) {
+    @Override
+    protected void dataItemRemoved(final Data<X, Y> item, final Series<X, Y> series) {
         final Node block = item.getNode();
         getPlotChildren().remove(block);
         removeDataItemFromDisplay(series, item);
     }
 
-    @Override protected void dataItemChanged(Data<X, Y> item) {
+    @Override
+    protected void dataItemChanged(Data<X, Y> item) {
     }
 
-    @Override protected  void seriesAdded(Series<X,Y> series, int seriesIndex) {
-        for (int j=0; j<series.getData().size(); j++) {
-            Data<X,Y> item = series.getData().get(j);
+    @Override
+    protected void seriesAdded(Series<X, Y> series, int seriesIndex) {
+        for (int j = 0; j < series.getData().size(); j++) {
+            Data<X, Y> item = series.getData().get(j);
             Node container = createContainer(series, seriesIndex, item, j);
             getPlotChildren().add(container);
         }
     }
 
-    @Override protected  void seriesRemoved(final Series<X,Y> series) {
-        for (XYChart.Data<X,Y> d : series.getData()) {
+    @Override
+    protected void seriesRemoved(final Series<X, Y> series) {
+        for (XYChart.Data<X, Y> d : series.getData()) {
             final Node container = d.getNode();
             getPlotChildren().remove(container);
         }
@@ -166,7 +192,7 @@ public class GanttChart<X,Y> extends XYChart<X,Y> {
     }
 
 
-    private Node createContainer(Series<X, Y> series, int seriesIndex, final Data<X,Y> item, int itemIndex) {
+    private Node createContainer(Series<X, Y> series, int seriesIndex, final Data<X, Y> item, int itemIndex) {
 
         Node container = item.getNode();
 
@@ -175,32 +201,33 @@ public class GanttChart<X,Y> extends XYChart<X,Y> {
             item.setNode(container);
         }
 
-        container.getStyleClass().add( getStyleClass( item.getExtraValue()));
+        container.getStyleClass().add(getStyleClass(item.getExtraValue()));
 
         return container;
     }
 
-    @Override protected void updateAxisRange() {
+    @Override
+    protected void updateAxisRange() {
         final Axis<X> xa = getXAxis();
         final Axis<Y> ya = getYAxis();
         List<X> xData = null;
         List<Y> yData = null;
-        if(xa.isAutoRanging()) xData = new ArrayList<X>();
-        if(ya.isAutoRanging()) yData = new ArrayList<Y>();
-        if(xData != null || yData != null) {
-            for(Series<X,Y> series : getData()) {
-                for(Data<X,Y> data: series.getData()) {
-                    if(xData != null) {
+        if (xa.isAutoRanging()) xData = new ArrayList<X>();
+        if (ya.isAutoRanging()) yData = new ArrayList<Y>();
+        if (xData != null || yData != null) {
+            for (Series<X, Y> series : getData()) {
+                for (Data<X, Y> data : series.getData()) {
+                    if (xData != null) {
                         xData.add(data.getXValue());
                         xData.add(xa.toRealValue(xa.toNumericValue(data.getXValue()) + getLength(data.getExtraValue())));
                     }
-                    if(yData != null){
+                    if (yData != null) {
                         yData.add(data.getYValue());
                     }
                 }
             }
-            if(xData != null) xa.invalidateRange(xData);
-            if(yData != null) ya.invalidateRange(yData);
+            if (xData != null) xa.invalidateRange(xData);
+            if (yData != null) ya.invalidateRange(yData);
         }
     }
 
