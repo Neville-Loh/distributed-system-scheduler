cdcfd2f698d568a7248deb9a0586cd1572c85094
jlee842 <jlee842@aucklanduni.ac.nz>
2021-08-20 01:08:03 +1200

    Final changes to csv
    I added some documentation and deleted some useless code. Should be ready to merge to master later.

--- src/main/java/raspberry/scheduler/io/Logger.java
+++ src/main/java/raspberry/scheduler/io/Logger.java
@@ -17,16 +17,21 @@ import au.com.bytecode.opencsv.CSVReader;
  * This class handles the overall statistics: start time, finish time and memory usage
  * for the program and exports it to a csv class for the report. It will compare the
  * milestone 1, program without multithreading and program with multithreading.
- * @author Jonathon
+ * @author Jonathon, Young
  */
 public class Logger {
-//    private double _dateTime;
-//    private String _inputFileName;
-//    private int _processorNumber;
-//    private double _duration;
+    // Output file declaration
     private static final String CSV_FILE = "src/test/resources/output/performanceData.csv";
 
-
+    /**
+     * This class collects the passed data from the main class and processes it
+     * for the CSV file before it is passed on.
+     * @param CLIConfig CLI Configuration object which contains input dotfile and
+     *                  number of processors
+     * @param startTime start time of algorithm
+     * @param currentTime finish time of algorithm
+     * @throws IOException for when a file is not called/created properly
+     */
     public static void log(CLIConfig CLIConfig, Double startTime, long currentTime) throws IOException {
         String[] _dataLines = new String[]{
                 java.time.LocalDateTime.now().toString(),
@@ -37,13 +42,16 @@ public class Logger {
     }
 
     /**
-     * Outputs the statistics to csv file.
-     * @throws IOException
+     * This method passes the logged statistics into a CSV file.
+     * @param input input data from logger
+     * @throws IOException for when a file is not called/created properly
      */
     public static void fileOutput(String[] input) throws IOException {
+        // Creates a writer object
         CSVWriter writer = new CSVWriter(new FileWriter(CSV_FILE, true));
-
+        // Writes the data into the writer
         writer.writeNext(input);
+        // Close the file
         writer.close();
     }
 }
